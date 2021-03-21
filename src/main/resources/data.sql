2ca442cf3324a1234bdf2770f2f82dfcccc3c138
unknown <waly@gmail.com>
2021-03-21 12:10:15 +0100

    Load data from data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,37 @@
+INSERT INTO FLIGHT values (1, 'BYD', 'KRK', current_timestamp+1, 999);
+INSERT INTO FLIGHT values (2, 'MON', 'SEA', current_timestamp+2, 998);
+INSERT INTO FLIGHT values (3, 'GDN', 'WAR', current_timestamp+3, 997);
+INSERT INTO FLIGHT values (4, 'LON', 'ROM', current_timestamp+4, 996);
+INSERT INTO FLIGHT values (5, 'PHO', 'DUB', current_timestamp, 995);
+
+
+INSERT INTO CARGO values (1);
+INSERT INTO CARGO values (2);
+INSERT INTO CARGO values (3);
+INSERT INTO CARGO values (4);
+INSERT INTO CARGO values (5);
+
+
+INSERT INTO CARGOES values (1, 201, 154, 'kg', 1);
+INSERT INTO CARGOES values (2, 201, 154, 'kg', 1);
+INSERT INTO CARGOES values (3, 999, 315, 'lb', 2);
+INSERT INTO CARGOES values (4, 331, 675, 'kg', 2);
+INSERT INTO CARGOES values (5, 678, 432, 'lb', 3);
+INSERT INTO CARGOES values (6, 100, 997, 'lb', 3);
+INSERT INTO CARGOES values (7, 351, 333, 'kg', 4);
+INSERT INTO CARGOES values (8, 678, 543, 'kg', 4);
+INSERT INTO CARGOES values (9, 881, 871, 'kg', 5);
+INSERT INTO CARGOES values (10, 207, 151, 'lb', 5);
+
+
+INSERT INTO Baggage values (1, 109, 688, 'lb', 1);
+INSERT INTO Baggage values (2, 775, 551, 'lb', 1);
+INSERT INTO Baggage values (3, 871, 888, 'kg', 2);
+INSERT INTO Baggage values (4, 177, 828, 'lb', 2);
+INSERT INTO Baggage values (5, 881, 885, 'kg', 3);
+INSERT INTO Baggage values (6, 991, 501, 'kg', 3);
+INSERT INTO Baggage values (7, 611, 838, 'kg', 4);
+INSERT INTO Baggage values (8, 198, 688, 'lb', 4);
+INSERT INTO Baggage values (9, 881, 888, 'kg', 5);
+INSERT INTO Baggage values (10, 961, 764, 'lb', 5);
+
